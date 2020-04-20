d1513cb5c51c7593dcc3399565a307bc82e9e4c6
Eduardo da Silva <eduardo.silva@ifc.edu.br>
2020-04-20 14:51:09 -0300

    backend: Pregnancy Diagnosis Model

--- backend/core/models/pregnancyDiagnosis.py
+++ backend/core/models/pregnancyDiagnosis.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-from backend.core.models import Sheep, User
+from .sheep import Sheep
+from .user import User
 
 
 class PregnancyDiagnosis(models.Model):
-    sheep = models.ForeignKey(Sheep, on_delete=models.PROTECT, related_name="pregnancy-diagnostics")
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="pregnancy-diagnostics")
+    sheep = models.ForeignKey(Sheep, on_delete=models.PROTECT, related_name="pregnancyDiagnostics")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="pregnancyDiagnostics")
     date = models.DateTimeField()
     diagnosis = models.BooleanField()
