e3865610ce88f3bb10c57272773d93bcf90cf8b5
Eduardo da Silva <eduardo.silva@ifc.edu.br>
2020-04-22 13:35:48 -0300

    Frontend? redirect to login on token expired

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,14 +1,5 @@
 export default function ({ $axios, redirect }) {
-  $axios.onRequest((config) => {
-    console.log("Making request to " + config.url);
-  });
-
-  $axios.onResponse((response) => {});
-
-  $axios.onError((error) => {
-    const code = parseInt(error.response && error.response.status);
-    if (code === 400) {
-      redirect("/400");
-    }
-  });
+  $axios.onRequest(config => {
+    console.log('Making request to ' + config.url)
+  })
 }
