e1c4a2a038cb2659fff0ccc99a5802e2d14fb915
Eduardo da Silva <eduardo.silva@ifc.edu.br>
2020-04-28 15:34:27 -0300

    Backend: update token lifetime

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ AUTH_USER_MODEL = "core.User"
 LOGIN_URL = "/api/v1/signin"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
 }
 
