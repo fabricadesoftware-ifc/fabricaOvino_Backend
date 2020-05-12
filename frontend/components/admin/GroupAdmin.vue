8568078ba8120740a8050ef5a04ff261926b1105
Eduardo da Silva <eduardo.silva@ifc.edu.br>
2020-05-12 13:45:06 -0300

    Fronted: group deletion function. Ref #12.

--- frontend/components/admin/GroupAdmin.vue
+++ frontend/components/admin/GroupAdmin.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <b-button
+      v-if="!deleteMode"
       type="is-primary"
       tag="nuxt-link"
       :to="localePath({ name: 'admin-group-add' })"
       icon-left="account-group"
     >{{$t('components.admin.groupadmin.add')}}</b-button>
+    <span v-else>
+      <b-button
+        type="is-danger"
+        @click="deleteGroup()"
+        icon-left="account-group"
+      >{{$t('components.admin.groupadmin.confirmDeletion')}} - {{ groupToDelete.name }}</b-button>
+    </span>
     <b-table striped :data="groups">
       <template slot-scope="props">
         <b-table-column
@@ -18,16 +26,24 @@
 
         <b-table-column field="actions" :label="$t('pages.admin.breed.table.actions')">
           <b-button type="is-warning" @click="editGroup(props.row)" icon-left="pencil"></b-button>
-          <b-button type="is-danger" icon-left="trash-can-outline"></b-button>
+          <b-button type="is-danger" @click="deleteGroup(props.row)" icon-left="trash-can-outline"></b-button>
         </b-table-column>
       </template>
     </b-table>
     <b-button
+      v-if="!deleteMode"
       type="is-primary"
       tag="nuxt-link"
       :to="localePath({ name: 'admin-group-add' })"
       icon-left="account-group"
     >{{$t('components.admin.groupadmin.add')}}</b-button>
+    <span v-else>
+      <b-button
+        type="is-danger"
+        @click="deleteGroup()"
+        icon-left="account-group"
+      >{{$t('components.admin.groupadmin.confirmDeletion')}} - {{ groupToDelete.name }}</b-button>
+    </span>
   </div>
 </template>
 
@@ -35,6 +51,8 @@
 export default {
   data() {
     return {
+      deleteMode: false,
+      groupToDelete: '',
       groups: [],
       columns: [
         {
@@ -59,6 +77,24 @@ export default {
         name: `admin-group-edit___${this.$i18n.locale}`,
         params: { group }
       })
+    },
+    deleteGroup(group=this.groupToDelete) {
+      if (!this.deleteMode) {
+        this.groupToDelete = group
+        this.deleteMode = true
+      } else {
+        let url = `/api/v1/groups/${this.groupToDelete.id}`
+        this.groupToDelete = ''
+        this.deleteMode = false
+        this.$axios.$delete(url).then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.$fetch()
+        }).catch(e =>{
+          for (var item in e.response.data) {
+            this.$toast.error(item + ': ' +  e.response.data[item])
+          }
+        })
+      }
     }
   }
 }
