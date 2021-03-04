75b404de7f717e88ac8efe17d9be23c3dc8b136e
Maria Eduarda Neves Correia <mariaae.correia@gmail.com>
2021-03-04 17:54:29 -0300

    Adição de "lots" no admin e na documentação

--- backend/core/router.py
+++ backend/core/router.py
@@ -1,6 +1,5 @@
-from rest_framework.routers import SimpleRouter
-
 from backend.core import views
+from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 
@@ -9,6 +8,7 @@ router.register(r"breeds", views.BreedViewSet, basename="breed")
 router.register(r"categories", views.CategoryViewSet, basename="category")
 router.register(r"feeds", views.FeedViewSet, basename="feed")
 router.register(r"groups", views.GroupViewSet, basename="group")
+router.register(r"lots", views.GroupViewSet, basename="lots")
 router.register(r"permissions", views.PermissionViewSet, basename="permission")
 router.register(r"pregnancy-diagnosis", views.PregnancyDiagnosisViewSet, basename="pregnancy-diagnosis")
 router.register(r"shearing", views.ShearingViewSet, basename="shearing")
