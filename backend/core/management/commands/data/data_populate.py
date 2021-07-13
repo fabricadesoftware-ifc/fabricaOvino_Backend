dbdf869ad71725819ee6928e2ea8e336b2728fc1
HenriqueHartmann <alemaohartmann@gmail.com>
2021-07-13 16:41:16 -0300

    Add new data types

---
+++ backend/core/management/commands/data/data_populate.py
@@ -0,0 +1,106 @@
+breeds = [
+    {
+        "name": "Dorper",
+        "nickname": "",
+    },
+    {
+        "name": "Merino",
+        "nickname": "",
+    },
+    {
+        "name": "Suffolk",
+        "nickname": "",
+    },
+    {
+        "name": "Valais Blacknose",
+        "nickname": "",
+    },
+    {
+        "name": "Priangan",
+        "nickname": "",
+    },
+    {
+        "name": "Camoronesa",
+        "nickname": "",
+    },
+    {
+        "name": "Ouessant",
+        "nickname": "",
+    },
+    {
+        "name": "Texel",
+        "nickname": "",
+    },
+    {
+        "name": "Awassi",
+        "nickname": "",
+    },
+    {
+        "name": "Caracul",
+        "nickname": "",
+    },
+    {
+        "name": "Ladoum",
+        "nickname": "",
+    },
+    {
+        "name": "Hampshire",
+        "nickname": "",
+    },
+    {
+        "name": "Herdwick",
+        "nickname": "",
+    },
+    {
+        "name": "Romanov",
+        "nickname": "",
+    },
+    {
+        "name": "Lacaune",
+        "nickname": "",
+    },
+]
+
+categorys = [
+    {
+        "name": "Branca",
+    },
+    {
+        "name": "Marrom",
+    },
+    {
+        "name": "Preta",
+    }
+]
+
+feeds = [
+    {
+        "name": "FORT SAL",
+        "description": "Suplementos Minerais",
+    },
+    {
+        "name": "AGROMIX",
+        "description": "Engorda",
+    },
+    {
+        "name": "MINER SAL",
+        "description": "Ração de pronto uso, auxilia no desenvolvimento de carcaça, ganho de peso, e aumento da taxa de prenhes",
+    },
+]
+
+users = [
+    {
+        "email": "a@gmail.com",
+        "password": "a",
+        "name": "Henrique Hartmann",
+        "username": "a@gmail.com",
+        "is_superuser": True,
+        "is_staff": True,
+    },
+    {
+        "email": "b@gmail.com",
+        "password": "b",
+        "name": "Kick Buttowiski",
+        "username": "b@gmail.com",
+    }
+]
